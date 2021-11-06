a7d6ec6146dd2a10927ef547de7a708b91706258
srngch <srngch.dev@gmail.com>
2021-11-06 17:45:14 +0900

    fix: STDIN to STDERR

--- src/util.c
+++ src/util.c
@@ -6,7 +6,7 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 01:29:47 by sarchoi           #+#    #+#             */
-/*   Updated: 2021/10/24 14:41:10 by sarchoi          ###   ########.fr       */
+/*   Updated: 2021/11/05 21:43:10 by sarchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	ft_exit_failure_with_msg(char *message)
 {
-	ft_putstr_fd(RED, STDIN_FILENO);
-	ft_putstr_fd("<ERROR> ", STDIN_FILENO);
-	ft_putstr_fd(message, STDIN_FILENO);
-	ft_putstr_fd("\n", STDIN_FILENO);
-	ft_putstr_fd(RESET, STDIN_FILENO);
+	ft_putstr_fd(RED, STDERR_FILENO);
+	ft_putstr_fd("<ERROR> ", STDERR_FILENO);
+	ft_putstr_fd(message, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	ft_putstr_fd(RESET, STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
 
